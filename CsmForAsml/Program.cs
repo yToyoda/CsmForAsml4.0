d036fc7573d1255a167f962c0f9c1ec624986a26
yToyoda <40904054+yToyoda@users.noreply.github.com>
2020-11-28 04:50:58 +0900

    Deploy Version 4.0

--- CsmForAsml/Program.cs
+++ CsmForAsml/Program.cs
@@ -16,13 +16,14 @@ namespace CsmForAsml {
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+            /*
                 .ConfigureAppConfiguration((context, config) =>
                 {
                 var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
                 config.AddAzureKeyVault(
                 keyVaultEndpoint,
                 new DefaultAzureCredential());
-                })
+                })  */
                 .ConfigureWebHostDefaults(webBuilder => {
                     webBuilder.UseStartup<Startup>();
                 });
