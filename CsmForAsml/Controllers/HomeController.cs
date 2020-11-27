5418f480de68e63fb99725f8c849d737dcf31402
yToyoda <40904054+yToyoda@users.noreply.github.com>
2020-11-27 11:17:25 +0900

    Register Page added

--- CsmForAsml/Controllers/HomeController.cs
+++ CsmForAsml/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace CsmForAsml.Controllers {
             return View();
         }
 
+        // [Authorize]
+        public IActionResult History() {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
